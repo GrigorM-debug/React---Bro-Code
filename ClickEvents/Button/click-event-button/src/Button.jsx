82c3cf56f21a1cd28e4947f3b7e5d61d30696bf4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-03-05 12:42:27 +0200

    Remove some lines of code

--- ClickEvents/Button/click-event-button/src/Button.jsx
+++ ClickEvents/Button/click-event-button/src/Button.jsx
@@ -5,29 +5,12 @@ function Button(){
 
     const imgPath = 'src/assets/gorilla.jpg';
 
-    // const buttonHandleClick = (e) => e.target.textContent = "Stop Clicking Me! 😑";
+    const buttonHandleClick = (e) => e.target.textContent = "Stop Clicking Me! 😑";
 
-    // const buttonHandleClick2 = (e) => e.target.style.display = "none";
-
-    const buttonHandleClick3 = () =>{
-        return(
-            <div>
-                <h3>Fuck You! {name}</h3>
-                <img src={imgPath} alt="Gorilla Picture" />
-            </div>
-        );
-    };
+    const buttonHandleClick2 = (e) => e.target.style.display = "none";
 
     return(
-        // <button onClick={(name) => buttonHandleClick3(name)}>Click Me! 😁</button>
-        <div>
-            <input
-                type="text"
-                placeholder="Enter your name"
-                onChange={(e) => setName(e.target.value)}
-            />
-            <button onClick={buttonHandleClick3}>Click Me! 😁</button>
-        </div>
+        <button onClick={(e) => buttonHandleClick(e)}>Click Me! 😁</button>
     );
 }
 
