613fdf7ef576980a8d416d7d510a8f315b8e5d37
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-03-05 12:39:18 +0200

    Buttons

--- ClickEvents/Button/click-event-button/src/Button.jsx
+++ ClickEvents/Button/click-event-button/src/Button.jsx
@@ -1,10 +1,33 @@
-function Button(){
-    const buttonHandleClick = (e) => e.target.textContent = "Stop Clicking Me! 😑";
+import React, { useState } from 'react';
 
-    const buttonHandleClick2 = (e) => e.target.style.display = "none";
+function Button(){
+    const [name, setName] = useState("");
+
+    const imgPath = 'src/assets/gorilla.jpg';
+
+    // const buttonHandleClick = (e) => e.target.textContent = "Stop Clicking Me! 😑";
+
+    // const buttonHandleClick2 = (e) => e.target.style.display = "none";
+
+    const buttonHandleClick3 = () =>{
+        return(
+            <div>
+                <h3>Fuck You! {name}</h3>
+                <img src={imgPath} alt="Gorilla Picture" />
+            </div>
+        );
+    };
 
     return(
-        <button onClick={(e) => buttonHandleClick2(e)}>Click Me! 😁</button>
+        // <button onClick={(name) => buttonHandleClick3(name)}>Click Me! 😁</button>
+        <div>
+            <input
+                type="text"
+                placeholder="Enter your name"
+                onChange={(e) => setName(e.target.value)}
+            />
+            <button onClick={buttonHandleClick3}>Click Me! 😁</button>
+        </div>
     );
 }
 
